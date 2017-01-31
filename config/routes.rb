c13bb9f48ac89e91ffd4399ae69cfea8e56072c9
LisaHernandez63 <Lisaleesa63@gmail.com>
2017-01-31 10:19:27 -0500

    add team show page and foundation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :teams, only: [:index]
+  resources :teams, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root "teams#index"
 end
