5cdd68709d00ca88f88ac60ba1cf97858bbc9e4b
LisaHernandez63 <Lisaleesa63@gmail.com>
2017-02-01 17:30:52 -0500

    added root to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root "teams#index"
   devise_for :users
   resources :teams, only: [:index, :show]
   resources :players, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root "teams#index"
 end
