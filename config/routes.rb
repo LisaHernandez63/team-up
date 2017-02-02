f39da79c87bea710073318e61e0090b748c5e7d9
LisaHernandez63 <Lisaleesa63@gmail.com>
2017-02-02 12:19:44 -0500

    added workouts to player show page,team add function

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "teams#index"
   devise_for :users
-  resources :teams, only: [:index, :show]
+  resources :teams, only: [:index, :show, :new, :create]
   resources :players, only: [:show]
   resources :workouts, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
