7360bc894ef77a5b51bfb0ab7afe2de4015c6ae3
LisaHernandez63 <Lisaleesa63@gmail.com>
2017-02-10 14:21:35 -0500

    made changes to nav bar

--- Procfile
+++ Procfile
@@ -1,6 +1,5 @@
 web: bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-development}
 
-
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
---
+++ spec/features/02_visitor_sees_page_of_show_team_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+feature "visitor sees details for a particular team" do
+  scenario "clicks link from index and sees info for that team" do
+  user1 = User.create(
+      email: 'lhdiff@yahoo.com',
+      encrypted_password: 'password',
+      username: 'Diff'
+    )
+
+  team1 = Team.create(
+    teamname: 'Lexington High Junior Varsity Boys Hockey',
+    coach: 'Matt Piccarillo',
+    schoolname: 'Lexington High School',
+    streetaddress: '251 Waltham Street',
+    town: 'Lexington',
+    state: 'MA',
+    zip: '02420',
+    img: 'IMG_2871.jpg',
+    user_id: user1.id,
+    description: 'A young hockey team with a lot of heart'
+  )
+
+
+    visit teams_path
+    expect(page).to have_content team1.teamname
+    click_link team1.teamname
+
+    expect(page).to have_content team1.teamname
+    expect(page).to have_content team1.description
+
+  end
+end
