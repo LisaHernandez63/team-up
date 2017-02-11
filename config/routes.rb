1295e7b3cde1e75b7ba28332e725d0f49db26906
LisaHernandez63 <Lisaleesa63@gmail.com>
2017-02-10 20:32:40 -0500

    fix devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root "pages#show"
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+        confirmations: 'users/confirmations'
+        omniauth: 'users/omniauth'
+        passwords: 'users/passwords'
+        unlocks: 'users/unlocks'
+      }
   resources :users, only: [:show]
   resources :pages, only: [:show]
   resources :teams, only: [:index, :show, :new, :create] do
