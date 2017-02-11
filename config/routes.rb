a294707a82d0676a606316b7d5298af3811a3b6a
LisaHernandez63 <Lisaleesa63@gmail.com>
2017-02-10 20:36:03 -0500

    changed devise route

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   root "pages#show"
   devise_for :users, controllers: {
         sessions: 'users/sessions'
-        confirmations: 'users/confirmations'
-        omniauth: 'users/omniauth'
-        passwords: 'users/passwords'
-        unlocks: 'users/unlocks'
       }
   resources :users, only: [:show]
   resources :pages, only: [:show]
